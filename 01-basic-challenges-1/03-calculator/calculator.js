13aa733064c6173589dff1491350c54ffe599ec5
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2025-03-24 22:25:44 -0600

    8. Calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,18 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+    case "-":
+      result = num1 - num2;
+    case "*":
+      result = num1 * num2;
+    case "/":
+      result = num1 / num2;
+    default:
+      throw new Error("Invalid operator");
+  }
+}
 
 module.exports = calculator;
