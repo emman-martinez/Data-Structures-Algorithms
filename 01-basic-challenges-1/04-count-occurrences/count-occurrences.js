4bf4351a967aed655ea380ce84a94a970b554d92
Emmanuel Martinez <emmanuel.martinez@medtrainer.com>
2025-03-24 22:38:00 -0600

    9. Count Occurences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,29 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = countOccurrences;
+
+// Case insensitive version
+// function countOccurrences(str, char) {
+//   const lowerStr = str.toLowerCase();
+//   const lowerChar = char.toLowerCase();
+
+//   let count = 0;
+
+//   for (let i = 0; i < lowerStr.length; i++) {
+//     if (lowerStr[i] === lowerChar) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
